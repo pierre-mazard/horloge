918c5469b115dd52ff14d9b63aaff24040e894c3
Drazam <146709444+pierre-mazard@users.noreply.github.com>
2023-11-28 10:43:05 +0100

    Création du dossier et du fichier.py du projet

---
+++ horloge.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Nov 28 2023
+
+@author: Mazard Pierre
+
+#horloge
+"""
+
+#Importation de fonctions externes (librairies) :
+    
+    
+#Définition locale de fonctions : 
+  
+
+#Déclaration des variables : 
+
+
+#Corps principal du programme : 
+
+
