0e8c69a4b74cce1cc3333085e6b23b6373e91e9a
Drazam <146709444+pierre-mazard@users.noreply.github.com>
2023-11-30 13:58:10 +0100

    ajout fonction am/pm

--- horloge.py
+++ horloge.py
@@ -9,48 +9,92 @@ Created on Tue Nov 28 2023
 
 #                      Importation de fonctions externes (librairies) :
 
-import time    
+import time  
+from datetime import datetime  
 import threading    
 #                          Définition locale de fonctions : 
         
                                     #Heure Locale
 def heure_locale():
-    print (""" 
+    if mode_affichage == "No":
+        print (""" 
+    
+    Voici l'heure locale au format hh:mm:ss
+            
+            """)  
+        while True:
+            t = time.localtime()
+            current_time = time.strftime("%Hh:%Mm:%Ss", t)
+            print("""              Il est actuellement :""", current_time, end="\r")
+            #Actualisation à chaque secondes 
+            time.sleep(1)
+    if mode_affichage == "Yes":
+        print ("""
+               
+Voici l'heure définie au format hh:mm:ss AM/PM
 
-Voici l'heure locale au format hh:mm:ss
-        
-        """)  
-    while True:
-        t = time.localtime()
-        current_time = time.strftime("%Hh:%Mm:%Ss", t)
-        print("""              Il est actuellement :""", current_time, end="\r")
-        #Actualisation à chaque secondes 
-        time.sleep(1)
+""")  
 
+        while True:
+            now = datetime.now()
+            current_time = now.strftime("%I:%M:%S %p")
+            print(f"""             Il est actuellement : {current_time}""", end="\r")
+            time.sleep(1)
                                     #Heure définie
 def afficher_heure(heure_tuple): 
     heures, minutes, secondes = heure_tuple
-    print (""" 
-
-Voici l'heure définie au format hh:mm:ss
-         
-         """)  
-    while True:
-        print(f"""             Il est actuellement : {heures:02d}h:{minutes:02d}m:{secondes:02d}s""", end="\r")
-        #Actualisation à chaque secondes
-        time.sleep(1)
-        secondes += 1
-        if secondes >= 60:
-            secondes = 0
-            minutes += 1
-        if minutes >= 60:
-            minutes = 0
-            heures += 1
-        if heures >= 24:
-            heures = 0
+    if mode_affichage == "No":
+        print (""" 
     
-                                   
-                                     #Alarme
+    Voici l'heure définie au format hh:mm:ss
+             
+             """)  
+        while True:
+            print(f"""             Il est actuellement : {heures:02d}h:{minutes:02d}m:{secondes:02d}s""", end="\r")
+            #Actualisation à chaque secondes
+            time.sleep(1)
+            secondes += 1
+            if secondes >= 60:
+                secondes = 0
+                minutes += 1
+            if minutes >= 60:
+                minutes = 0
+                heures += 1
+            if heures >= 24:
+                heures = 0
+        
+    if mode_affichage == "Yes":
+           print (""" 
+    
+       Voici l'heure définie au format hh:mm:ss AM/PM
+                
+                """)  
+           while True and heures <= 12 :
+               print(f"""             Il est actuellement : {heures:02d}h:{minutes:02d}m:{secondes:02d}s AM""", end="\r")
+               #Actualisation à chaque secondes
+               time.sleep(1)
+               secondes += 1
+               if secondes >= 60:
+                   secondes = 0
+                   minutes += 1
+               if minutes >= 60:
+                   minutes = 0
+                   heures += 1
+               if heures >= 12:
+                   heures = 0                            
+           while True and heures >= 12 :
+                print(f"""             Il est actuellement : {heures:02d}h:{minutes:02d}m:{secondes:02d}s PM""", end="\r")
+                #Actualisation à chaque secondes
+                time.sleep(1)
+                secondes += 1
+                if secondes >= 60:
+                    secondes = 0
+                    minutes += 1
+                if minutes >= 60:
+                    minutes = 0
+                    heures += 1
+                if heures >= 12:
+                    heures = 0                                 #Alarme
                                     
 def regler_alarme(heure_alarme):
     def alarme():
@@ -63,8 +107,7 @@ def regler_alarme(heure_alarme):
                       ==> ! ALARME ! <==
                       """)
                 break
-            time.sleep(1)
-         #while regler_heure == "No"   
+            time.sleep(1) 
     # Crée un nouveau thread pour exécuter la fonction d'alarme
     thread_alarme = threading.Thread(target=alarme)
     # Démarre le thread
@@ -80,12 +123,21 @@ Souhaitez-vous afficher l'heure locale ou définir une heure ?
 
 ====> : """)
 
-alarme = input ("""
+alarme = input("""
 Souhaitez-vous définir une alarme ?
 
 =>  Entrez 'Yes' pour définir une alarme.
 
 =>  Entrez 'No' pour passer cette étape.                     
+
+====> : """)
+
+mode_affichage = input(f"""
+Souhaitez-vous l'heure au format 12 ou 24 ?
+
+=>  Entrez 'Yes' pour un format 12h.
+
+=>  Entrez 'No' pour un format 24h.                     
 
 ====> : """)
 
